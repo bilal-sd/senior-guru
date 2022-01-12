06784f5aa4463b44db6a8117f84ce716dcb2d923
Aashish Nayak <95460946+aashish-scrum@users.noreply.github.com>
2022-01-12 17:16:24 +0530

    Update breadcrumb.blade.php

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -9,23 +9,9 @@
               {{ $slot ?? ''}}
           </ol>
         </div>
-<<<<<<< HEAD
-        
-        
-        <div class="col-lg-6">
-          <!-- Bookmark Start-->
-          <div class="bookmark">
-            <ul>
-              <li>
-                {{ $right_button ?? '' }}
-              </li>
-            </ul>
-          </div>
-          <!-- Bookmark Ends-->
-=======
+
         <div class="col-lg-6 text-end">
             {{$right_button ?? ''}}
->>>>>>> ed12f0e457440bc355645b7ee602dfee58335f92
         </div>
       </div>
     </div>
